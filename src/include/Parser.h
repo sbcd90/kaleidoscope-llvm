42bb8e3d167b4c9b4ac0da1e4af63a6bb0fb25f9
Subhobrata Dey <sbcd90@gmail.com>
2022-07-06 00:44:08 -0700

    initial commit

--- src/include/Parser.h
+++ src/include/Parser.h
@@ -10,6 +10,49 @@ static int getNextToken() {
 
 static std::map<char, int> binOpPrecedence;
 
+std::unique_ptr<ast::ExprAST> logError(std::string s) {
+    std::cout << "Error: " << s << std::endl;
+    return nullptr;
+}
+
+std::unique_ptr<ast::PrototypeAST> logErrorP(std::string s) {
+    logError(s);
+    return nullptr;
+}
+
+static std::unique_ptr<ast::PrototypeAST> parsePrototype() {
+    using namespace std::string_literals;
+    if (curTok != tokIdentifier) {
+        return logErrorP("Expected function name in prototype"s);
+    }
+
+    auto fnName = identifierStr;
+    getNextToken();
+
+    if (curTok != '(') {
+        logErrorP("Expected '(' in prototype"s);
+    }
+
+    std::vector<std::string> argNames{};
+    while (getNextToken() == tokIdentifier) {
+        argNames.push_back(identifierStr);
+    }
+
+    if (curTok != ')') {
+        logErrorP("Expected ')' in prototype"s);
+    }
+    getNextToken();
+    return std::make_unique<ast::PrototypeAST>(fnName, std::move(argNames));
+}
+
+static std::unique_ptr<ast::FunctionAST> parseDefinition() {
+    getNextToken();
+    auto proto = parsePrototype();
+    if (proto == nullptr) {
+        return nullptr;
+    }
+}
+
 static void handleDefinition() {
 
 }
